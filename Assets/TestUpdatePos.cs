927c4a4a27ee3f6a85f265fb0d51b18cfec6db49
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-06-02 14:41:03 +0900

    Orbit Cube

---
+++ Assets/TestUpdatePos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUpdatePos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(Camera.main.transform.forward * 3 * Time.deltaTime);
+        transform.RotateAround(Camera.main.transform.position,Vector3.up, 10*Time.deltaTime);
+	}
+}
